c78e4d33650905434ff99af329894f70b1de164e
Tanookh <Tanookhkabishi@gmail.com>
2022-03-16 16:05:07 +0200

    Added more functions

--- run.py
+++ run.py
@@ -3,3 +3,5 @@ from website.website import Website
 
 with Website() as bot:
     bot.land_first_page()
+    bot.change_currency(currency='USD')
+    bot.change_country(country='en-us')
--- website/website.py
+++ website/website.py
@@ -1,11 +1,8 @@
 import website.constants as const
 import os
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 
 # A class and functions for the webpage
-
-
 class Website(webdriver.Chrome):
 
     # Constructor for Website that gives the driver location and runs it
@@ -14,8 +11,8 @@ class Website(webdriver.Chrome):
         self.teardown = teardown
         os.environ['PATH'] += self.driver_path
         super(Website, self).__init__()
-        self.implicitly_wait(15)
-        self.maximize_window()
+        self.implicitly_wait(15) # Wait for the element to appear or wait 15 seconds
+        self.maximize_window() # Start with a full screen
 
     # Choose if to exit the webpage or not after running
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -25,3 +22,25 @@ class Website(webdriver.Chrome):
     # For opening the web page
     def land_first_page(self):
         self.get(const.BASE_URL)
+
+    # Change the currency
+    def change_currency(self, currency=None):
+        currency_element = self.find_element_by_css_selector(
+            'button[data-tooltip-text="Choose your currency"]'
+        )
+        currency_element.click()
+        selected_currency_element = self.find_element_by_css_selector(
+            f'a[data-modal-header-async-url-param*="selected_currency={currency}"]'
+        )
+        selected_currency_element.click()
+
+    # Change the country
+    def change_country(self, country=None):
+        country_element = self.find_element_by_css_selector(
+            'button[data-tooltip-text="Choose your language"]'
+        )
+        country_element.click()
+        selected_country_element = self.find_element_by_css_selector(
+            f'a[data-lang="{country}"]'
+        )
+        selected_country_element.click()
